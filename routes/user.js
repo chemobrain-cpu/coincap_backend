629907018e72ca4a9c50cf5bd0e980eeca0be37d
arierhiprecious@gmail.com <arierhiprecious@gmail.com>
2022-11-01 11:01:46 -0700

    latest commit

--- routes/user.js
+++ routes/user.js
@@ -83,7 +83,8 @@ router.post("/auth/login",
 router.patch("/auth/credentials",verifyToken,updateCredentials)
 
 router.delete('/auth/closemyaccount',verifyToken,closeUserAccount)
-router.get('/auth/getallusers',(req,res)=>{
+
+router.get('/auth/getallusers',async(req,res)=>{
     let allUser = await User.find()
     res.status(200).json({user:allUser})
 })
