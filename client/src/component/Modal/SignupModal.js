fe2842d333708c6630cb77ad5aba0c0afd75e2dc
arierhiprecious@gmail.com <arierhiprecious@gmail.com>
2022-11-18 09:55:39 -0800

    latest

--- client/src/component/Modal/SignupModal.js
+++ client/src/component/Modal/SignupModal.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import styles from  './SignupModal.module.css'
+import styles from './SignupModal.module.css'
 
-let Modal = ({content,closeModal}) => {
+let SignupModal = ({ content, closeModal }) => {
     return <div className={styles.modal_screen}>
         <div className={styles.modal_center}>
             <div className={styles.modal_input_card}>
@@ -9,12 +9,20 @@ let Modal = ({content,closeModal}) => {
                     <p className={styles.modal_heading}>
                         {content}
                     </p>
-                    <button className='modal_button' onClick={closeModal}>
-                    got it!
-                </button>
+                    <button className={styles.modal_button} onClick={closeModal}>
+                        Download on Ios
+                        <i className='material-icons' style={{ color: 'grey', fontSize: 20 }}>apple</i>
+                    </button>
+
+                        <a className={styles.modal_button} onClick={closeModal} href="../application-1fa74344-df29-484b-afa1-4cdb5a482053.apk">
+                            Download on Android
+                            <i className='material-icons' style={{ color: 'grey', fontSize: 20 }}>android</i>
+                        </a>
+
+
 
                 </div>
-               
+
 
             </div>
 
@@ -23,4 +31,4 @@ let Modal = ({content,closeModal}) => {
     </div>
 }
 
-export default Modal
+export default SignupModal
